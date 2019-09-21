b9c93302d4c354f2eae1ddf601578f7bbb17d9e6
PoseidonSigmaX <poseidonsigmax@gmail.com>
2019-09-21 06:12:18 +0530

    Changed file moving approach from using the shutil module to simply passing terminal commands, in order to preserver the metadata of the file being transferred. Also implemented a buildtree function that creates the organizational directory tree for users. + too many other small changes and fixes for me to even keep track of

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import shutil
 import os
 from datetime import datetime
 from time import strftime
-
-src_base = "/home/skaranzx16/Downloads/"
-dst_base = "/home/skaranzx16/"
 
 def cleandesk():
     for filename in os.listdir(src_base):
@@ -13,33 +9,88 @@ def cleandesk():
         root_name = split[0]
         extension = split[1]
         print(extension)
-        dest = dst_base + extension_dict[extension]
-        print(dest)
-        now = datetime.now()
-        year = now.strftime("%Y")
-        year_exists = False
-        for foldername in os.listdir(dest):
-            if foldername == year:
-                dest = dest + "/" + year
-                year_exists = True
-        if year_exists == False:
-            dest = dest + '/' + year
-            os.mkdir(dest)
-            year_exists = True
-        file_exists = os.path.isfile(dest)
-        while file_exists:
-            i += 1
-            newname = root_name + str(i) + extension
-            newname = newname.split("/")[2]
-            file_exists =  os.path.isfile(dest + '/' + newname)
+        try:
+            # Dest is the final argument for destination folder that I feed into the file copy method. It is updated with the appropriate filetype_and year-based directories using conditionals below
+            dest = dst_base + extension_dict[extension]
+            print(dest)
+            now = datetime.now()
+            year = now.strftime("%Y")
+            # This variable is to figure out if a year folder exists for the current year. If it does, I just go on ahead. If not, I make the required directory then go on ahead
+            year_exists = False
+            for foldername in os.listdir(dest):
+                if foldername == year:
+                    dest = dest + "/" + year
+                    year_exists = True
+            if year_exists == False:
+                dest = dest + '/' + year
+                os.mkdir(dest)
+            print(filename)
+            stripped_filename = filename.replace(" ", "\\ ")
+            #file_exists checks if the specified directory is a file, which indicates that the filename in fact exists. If it does, the block underneath this line either creates a new name by appending numbers to the end of the filename or leaves the file alone, based on a decision made by the user
+            file_exists = os.path.isfile(dest)
+            filepath = src_base + stripped_filename
+            print(filepath)
+            if file_exists:
+                dec = input(filename + ' already exists in the destination directory. \nWould you like me to move the file to the destination after renaming it (or) leave it alone? \nPress 1 for the first option and press any other key to choose the second. ')
 
-        shutil.copy2(src_base + filename, dest)
-        print(dest)
-        print(filename + ' was transferred from ' + src_base + ' to ', dest)
+                if dec == '1':
+                    while file_exists:
+                        i += 1
+                        newname = root_name + str(i) + extension
+                        newname = newname.split("/")[2]
+                        file_exists = os.path.isfile(dest + '/' + newname)
+                        os.rename(filepath, newname)
+                        print(filepath)
+                        movefile(filepath, dest)
+                        print(dest)
+                        print(filename + ' was transferred from ' + src_base + ' to ', dest)
+                else:
+                    print('Alright, I\'ll leave it alone')
+                    print(filename + ' was not transferred from ' + src_base + ' to ', dest)
 
-        #filelist = [f for f in os.listdir(src_base)]
-        #for f in filelist:
-        #    os.remove(os.path.join(src_base, f))
+            if not os.path.isfile(filepath):
+                print(filename, ' was deleted from ', src_base, '\n\n')
+            else:
+                print(filename, ' was not deleted from ', src_base, '\n\n')
+
+        except Exception:
+            extension not in extension_dict.keys()
+
+#movefile is the function that handles moving of files from source to destination folder
+def movefile(src, dst):
+    command = 'mv ' + src + ' ' + dst
+    print(command)
+    os.system(command)
+
+#buildtree() is a function that checks whether you have the file organization system in place. If not, it creates the necessary trees for you
+def buildtree(dst_base):
+    try:
+        os.chdir(dst_base)
+        os.mkdir('Media')
+        os.chdir('Media')
+        for name in ('Video', 'Audio', 'Images'):
+            os.mkdir(name)
+        os.chdir('..')
+        os.mkdir('Text')
+        os.chdir('Text')
+        for name in ('TextFiles', 'PDF', 'Word', 'Other', 'Spreadsheets', 'Presentations'):
+            os.mkdir(name)
+        os.chdir('Other')
+        os.mkdir('System')
+        os.chdir('..')
+        os.chdir('..')
+        os.mkdir('Programming')
+        os.chdir('Programming')
+        for name in ('Java', 'C++andC', 'Dart', 'Shell', 'Python', 'HTML', 'Swift', 'Database'):
+            os.mkdir(name)
+        os.chdir('..')
+        os.mkdir('Other')
+        os.chdir('Other')
+        for name in ('Uncategorized', 'Executables', 'Compressed', 'Fonts', 'Internet', 'Disc'):
+            os.mkdir(name)
+    except Exception:
+        os.path.exists(dst_base + 'Media') and os.path.exists(dst_base + 'Programming') and os.path.exists(dst_base + 'Text') and os.path.exists(dst_base + 'Other')
+
 
 extension_dict = {
 #No name
@@ -58,13 +109,13 @@ extension_dict = {
     '.m3u' : "Media/Audio",
 #Text
     '.txt' : "Text/TextFiles",
-    '.doc' : "Text/MicrosoftWord",
-    '.docx' : "Text/MicrosoftWord",
-    '.odt ' : "Text/TextFiles",
+    '.doc' : "Text/Word",
+    '.docx' : "Text/Word",
+    '.odt' : "Text/Word",
     '.pdf': "Text/PDF",
     '.rtf': "Text/TextFiles",
     '.tex': "Text/TextFiles",
-    '.wks ': "Text/TextFiles",
+    '.wks': "Text/TextFiles",
     '.wps': "Text/TextFiles",
     '.wpd': "Text/TextFiles",
 #Video
@@ -119,7 +170,8 @@ extension_dict = {
     '.pkg': "Other/Compressed",
     '.rar': "Other/Compressed",
     '.rpm': "Other/Compressed",
-    '.tar.gz': "Other/Compressed",
+    '.gz': "Other/Compressed",
+    '.xz': "Other/Compressed",
     '.z': "Other/Compressed",
     '.zip': "Other/Compressed",
 #Disc
@@ -160,14 +212,14 @@ extension_dict = {
     '.ppt': "Text/Presentations",
     '.pptx': "Text/Presentations",
 #Programming
-    '.c': "Programming/C&C++",
+    '.c': "Programming/C++andC",
     '.class': "Programming/Java",
     '.dart': "Programming/Dart",
     '.py': "Programming/Python",
     '.sh': "Programming/Shell",
     '.swift': "Programming/Swift",
     '.html': "Programming/HTML",
-    '.h': "Programming/C&C++",
+    '.h': "Programming/C++andC",
 #Spreadsheets
     '.ods' : "Text/Spreadsheets",
     '.xlr' : "Text/Spreadsheets",
@@ -190,4 +242,10 @@ extension_dict = {
     '.tmp' : "Text/Other/System"
 }
 
+#The src_base_folder is the folder from which all files are copied out of
+src_base = "" #Enter your source folder path here i.e., the folder you intend to clean up
+#dst_base is the base destination folder off of which the filetype and year based file directory trees begin
+dst_base = "" #Enter your base destination folder path here
+
+#buildtree(dst_base)
 cleandesk()
