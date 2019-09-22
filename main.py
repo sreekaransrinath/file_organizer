42718cb012126862bbe890e6971942077a79ed07
Sreekaran Srinath <50989977+skaranzx16@users.noreply.github.com>
2019-09-22 17:24:19 +0530

    Update main.py
    Bug fixes

--- main.py
+++ main.py
@@ -2,34 +2,31 @@ import os
 from datetime import datetime
 from time import strftime
 
+
 def cleandesk():
     for filename in os.listdir(src_base):
         i = 1
         split = os.path.splitext(src_base + filename)
         root_name = split[0]
         extension = split[1]
-        print(extension)
+        print('The file ', root_name, ' is of the type ', extension)
         try:
             # Dest is the final argument for destination folder that I feed into the file copy method. It is updated with the appropriate filetype_and year-based directories using conditionals below
             dest = dst_base + extension_dict[extension]
-            print(dest)
+            print('The destination folder based on file type is ', dest)
             now = datetime.now()
             year = now.strftime("%Y")
-            # This variable is to figure out if a year folder exists for the current year. If it does, I just go on ahead. If not, I make the required directory then go on ahead
-            year_exists = False
-            for foldername in os.listdir(dest):
-                if foldername == year:
-                    dest = dest + "/" + year
-                    year_exists = True
-            if year_exists == False:
-                dest = dest + '/' + year
-                os.mkdir(dest)
-            print(filename)
+            print('The year is', year)
+
+            dest = dest + "/" + year
+            print('The file name is ', filename)
             stripped_filename = filename.replace(" ", "\\ ")
-            #file_exists checks if the specified directory is a file, which indicates that the filename in fact exists. If it does, the block underneath this line either creates a new name by appending numbers to the end of the filename or leaves the file alone, based on a decision made by the user
+            print('The stripped file name is ', stripped_filename)
+            # file_exists checks if the specified directory is a file, which indicates that the filename in fact exists. If it does, the block underneath this line either creates a new name by appending numbers to the end of the filename or leaves the file alone, based on a decision made by the user
             file_exists = os.path.isfile(dest)
+            print('File exists is ', file_exists)
             filepath = src_base + stripped_filename
-            print(filepath)
+            print('The final source file path to be passed as argument is ', filepath)
             if file_exists:
                 dec = input(filename + ' already exists in the destination directory. \nWould you like me to move the file to the destination after renaming it (or) leave it alone? \nPress 1 for the first option and press any other key to choose the second. ')
 
@@ -40,18 +37,16 @@ def cleandesk():
                         newname = newname.split("/")[2]
                         file_exists = os.path.isfile(dest + '/' + newname)
                         os.rename(filepath, newname)
-                        print(filepath)
+                        print('Trial number ', i, 'The modified source filepath is ', filepath)
                         movefile(filepath, dest)
                         print(dest)
-                        print(filename + ' was transferred from ' + src_base + ' to ', dest)
+                        print(filename + ' was transferred from ' + src_base + ' to ', dest, 'A name clash occurred in the process but was fixed.')
                 else:
                     print('Alright, I\'ll leave it alone')
                     print(filename + ' was not transferred from ' + src_base + ' to ', dest)
-                    
-            if file_exists == False:
-                print(filepath)
+
+            if file_exists == False and os.path.isfile(filepath):
                 movefile(filepath, dest)
-                print(dest)
                 print(filename + ' was transferred from ' + src_base + ' to ', dest)
 
             if not os.path.isfile(filepath):
@@ -62,50 +57,56 @@ def cleandesk():
         except Exception:
             extension not in extension_dict.keys()
 
-#movefile is the function that handles moving of files from source to destination folder
+
+# movefile is the function that handles moving of files from source to destination folder
 def movefile(src, dst):
     command = 'mv ' + src + ' ' + dst
     print(command)
     os.system(command)
 
-#buildtree() is a function that checks whether you have the file organization system in place. If not, it creates the necessary trees for you
+
+# buildtree() is a function that checks whether you have the file organization system in place. If not, it creates the necessary trees for you
 def buildtree():
+    now = datetime.now()
+    year = now.strftime('%Y')
     for path in extension_dict.values():
-        command = dst_base + path
+        command = dst_base + path + '/' + year + '/'
         try:
-            print(command)
-            os.makedirs(command)
+            if not os.path.exists(command):
+                print('Does the path exist?', os.path.exists(command))
+                print(command)
+                os.makedirs(command)
         except Exception:
             os.path.exists(command)
 
 
 extension_dict = {
-#No name
-    'noname' : "Other/Uncategorized",
-#Audio
-    '.aif' : "Media/Audio",
-    '.cda' : "Media/Audio",
-    '.mid' : "Media/Audio",
-    '.midi' : "Media/Audio",
-    '.mp3' : "Media/Audio",
-    '.mpa' : "Media/Audio",
-    '.ogg' : "Media/Audio",
-    '.wav' : "Media/Audio",
-    '.wma' : "Media/Audio",
-    '.wpl' : "Media/Audio",
-    '.m3u' : "Media/Audio",
-#Text
-    '.txt' : "Text/TextFiles",
-    '.doc' : "Text/Word",
-    '.docx' : "Text/Word",
-    '.odt' : "Text/Word",
+    # No name
+    'noname': "Other/Uncategorized",
+    # Audio
+    '.aif': "Media/Audio",
+    '.cda': "Media/Audio",
+    '.mid': "Media/Audio",
+    '.midi': "Media/Audio",
+    '.mp3': "Media/Audio",
+    '.mpa': "Media/Audio",
+    '.ogg': "Media/Audio",
+    '.wav': "Media/Audio",
+    '.wma': "Media/Audio",
+    '.wpl': "Media/Audio",
+    '.m3u': "Media/Audio",
+    # Text
+    '.txt': "Text/TextFiles",
+    '.doc': "Text/Word",
+    '.docx': "Text/Word",
+    '.odt': "Text/Word",
     '.pdf': "Text/PDF",
     '.rtf': "Text/TextFiles",
     '.tex': "Text/TextFiles",
     '.wks': "Text/TextFiles",
     '.wps': "Text/TextFiles",
     '.wpd': "Text/TextFiles",
-#Video
+    # Video
     '.3g2': "Media/Video",
     '.3gp': "Media/Video",
     '.avi': "Media/Video",
@@ -121,7 +122,7 @@ extension_dict = {
     '.swf': "Media/Video",
     '.vob': "Media/Video",
     '.wmv': "Media/Video",
-#Images
+    # Images
     '.ai': "Media/Images",
     '.bmp': "Media/Images",
     '.gif': "Media/Images",
@@ -135,7 +136,7 @@ extension_dict = {
     '.tif': "Media/Images",
     '.tiff': "Media/Images",
     '.CR2': "Media/Images",
-#Internet
+    # Internet
     '.asp': "Other/Internet",
     '.aspx': "Other/Internet",
     '.cer': "Other/Internet",
@@ -150,7 +151,7 @@ extension_dict = {
     '.php': "Other/Internet",
     '.rss': "Other/Internet",
     '.xhtml': "Other/Internet",
-#Compressed
+    # Compressed
     '.7z': "Other/Compressed",
     '.arj': "Other/Compressed",
     '.deb': "Other/Compressed",
@@ -161,13 +162,14 @@ extension_dict = {
     '.xz': "Other/Compressed",
     '.z': "Other/Compressed",
     '.zip': "Other/Compressed",
-#Disc
+    '.tgz': "Other/Compressed",
+    # Disc
     '.bin': "Other/Disc",
     '.dmg': "Other/Disc",
     '.iso': "Other/Disc",
     '.toast': "Other/Disc",
     '.vcd': "Other/Disc",
-#Data
+    # Data
     '.csv': "Programming/Database",
     '.dat': "Programming/Database",
     '.db': "Programming/Database",
@@ -179,7 +181,7 @@ extension_dict = {
     '.tar': "Programming/Database",
     '.xml': "Programming/Database",
     '.json': "Programming/Database",
-#Executables
+    # Executables
     '.apk': "Other/Executables",
     '.bat': "Other/Executables",
     '.com': "Other/Executables",
@@ -187,18 +189,18 @@ extension_dict = {
     '.gadget': "Other/Executables",
     '.jar': "Other/Executables",
     '.wsf': "Other/Executables",
-#Fonts
+    # Fonts
     '.fnt': "Other/Fonts",
     '.fon': "Other/Fonts",
     '.otf': "Other/Fonts",
     '.ttf': "Other/Fonts",
-#Presentations
+    # Presentations
     '.key': "Text/Presentations",
     '.odp': "Text/Presentations",
     '.pps': "Text/Presentations",
     '.ppt': "Text/Presentations",
     '.pptx': "Text/Presentations",
-#Programming
+    # Programming
     '.c': "Programming/C++andC",
     '.class': "Programming/Java",
     '.dart': "Programming/Dart",
@@ -207,32 +209,32 @@ extension_dict = {
     '.swift': "Programming/Swift",
     '.html': "Programming/HTML",
     '.h': "Programming/C++andC",
-#Spreadsheets
-    '.ods' : "Text/Spreadsheets",
-    '.xlr' : "Text/Spreadsheets",
-    '.xls' : "Text/Spreadsheets",
-    '.xlsx' : "Text/Spreadsheets",
-#System
-    '.bak' : "Text/Other/System",
-    '.cab' : "Text/Other/System",
-    '.cfg' : "Text/Other/System",
-    '.cpl' : "Text/Other/System",
-    '.cur' : "Text/Other/System",
-    '.dll' : "Text/Other/System",
-    '.dmp' : "Text/Other/System",
-    '.drv' : "Text/Other/System",
-    '.icns' : "Text/Other/System",
-    '.ini' : "Text/Other/System",
-    '.lnk' : "Text/Other/System",
-    '.msi' : "Text/Other/System",
-    '.sys' : "Text/Other/System",
-    '.tmp' : "Text/Other/System"
+    # Spreadsheets
+    '.ods': "Text/Spreadsheets",
+    '.xlr': "Text/Spreadsheets",
+    '.xls': "Text/Spreadsheets",
+    '.xlsx': "Text/Spreadsheets",
+    # System
+    '.bak': "Text/Other/System",
+    '.cab': "Text/Other/System",
+    '.cfg': "Text/Other/System",
+    '.cpl': "Text/Other/System",
+    '.cur': "Text/Other/System",
+    '.dll': "Text/Other/System",
+    '.dmp': "Text/Other/System",
+    '.drv': "Text/Other/System",
+    '.icns': "Text/Other/System",
+    '.ini': "Text/Other/System",
+    '.lnk': "Text/Other/System",
+    '.msi': "Text/Other/System",
+    '.sys': "Text/Other/System",
+    '.tmp': "Text/Other/System"
 }
 
-#The src_base_folder is the folder from which all files are copied out of
-src_base = "" #Enter your source folder path here i.e., the folder you intend to clean up
-#dst_base is the base destination folder off of which the filetype and year based file directory trees begin
-dst_base = "" #Enter your base destination folder path here
+# The src_base_folder is the folder from which all files are copied out of
+src_base = "/home/skaranzx16/Downloads/"  # Enter your source folder path here i.e., the folder you intend to clean up
+# dst_base is the base destination folder off of which the filetype and year based file directory trees begin
+dst_base = "/home/skaranzx16/"  # Enter your base destination folder path here
 
-#buildtree(dst_base)
+buildtree()
 cleandesk()
