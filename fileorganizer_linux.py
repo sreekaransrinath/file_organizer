0265ae104796a307ee199ee4a062237fdd9f556f
Sreekaran Srinath <50989977+skaranzx16@users.noreply.github.com>
2019-09-23 22:38:57 +0530

    Only for linux
    Since Windows and Linux handle and take very different shell commands, decided to make a separate script for both.
    
    At some point in the future, I plan on detecting the OS and including both scripts in two if blocks, but for now, I'm working on some more important (imo) features. 
    
    Oh, and also added metadata reading to figure out when a file was modified last and determine its appropriate year folder from that.
    
    It's too difficult to read creation date in Linux, so had to resort to reading the modification date.
    
    The Windows version of this script will be up positively by tomorrow, and that will involve reading creation date rather than modification date.

---
+++ fileorganizer_linux.py
@@ -0,0 +1,93 @@
+import os
+from datetime import datetime
+from time import strftime
+import extensiondict
+
+def cleandesk():
+    for filename in os.listdir(src_base):
+        i = 1
+        split = os.path.splitext(src_base + filename)
+        root_name = split[0]
+        extension = split[1]
+        print('The file ', root_name, ' is of the type ', extension)
+        try:
+            # Dest is the final argument for destination folder that I feed into the file copy method. It is updated with the appropriate filetype_and year-based directories using conditionals below
+            dest = dst_base + extensiondict.extension_dict[extension]
+            print('The destination folder based on file type is ', dest)
+            mdate = os.path.getmtime(src_base + filename)
+            year = datetime.fromtimestamp(int(mdate)).strftime("%Y") 
+            print('The year of modification for this file is', year)
+            dest = dest + "/" + year
+            year_exists = os.path.exists(dest)
+            if not year_exists:
+                os.makedirs(dest)
+            print('The file name is ', filename)
+            stripped_filename = filename.replace(" ", "\\ ")
+            print('The stripped file name is ', stripped_filename)
+            # file_exists checks if the specified directory is a file, which indicates that the filename in fact exists. If it does, the block underneath this line either creates a new name by appending numbers to the end of the filename or leaves the file alone, based on a decision made by the user
+            file_exists = os.path.isfile(dest + '/' + filename)
+            print('File exists is ', file_exists)
+            filepath = src_base + stripped_filename
+            print('The final source file path to be passed as argument is ', filepath)
+            if file_exists:
+                dec = input(filename + ' already exists in the destination directory. \nWould you like me to move the file to the destination after renaming it (or) leave it alone? \nPress 1 for the first option and press any other key to choose the second. ')
+
+                if dec == '1':
+                    while file_exists:
+                        i += 1
+                        newname = root_name + str(i) + extension
+                        newname = newname.split("/")[3]
+                        file_exists = os.path.isfile(dest + '/' + newname)
+                        os.rename(filepath, newname)
+                        print('Trial number ', i, 'The modified source filepath is ', filepath, 'And the new name is ', newname)
+                        movefile(filepath, dest)
+                        print(dest)
+                        print(filename + ' was transferred from ' + src_base + ' to ', dest, 'A name clash occurred in the process but was fixed.')
+                
+                else:
+                    print('Alright, I\'ll leave it alone')
+                    print(filename + ' was not transferred from ' + src_base + ' to ', dest)
+
+            if file_exists == False and os.path.isfile(src_base + filename):
+                movefile(filepath, dest)
+                print(filename + ' was transferred from ' + src_base + ' to ', dest)
+
+            if not os.path.isfile(src_base + filename):
+                print(filename, ' was deleted from ', src_base, '\n\n')
+            
+            else:
+                print(filename, ' was not deleted from ', src_base, '\n\n')
+
+        except Exception:
+            extension not in extensiondict.extension_dict.keys()
+
+
+# movefile is the function that handles moving of files from source to destination folder
+def movefile(src, dst):
+    command = 'mv ' + src + ' ' + dst
+    print('The command to be passed to shell is ', command)
+    os.system(command)
+
+
+# buildtree() is a function that checks whether you have the file organization system in place. If not, it creates the necessary trees for you
+def buildtree():
+    now = datetime.now()
+    year = now.strftime('%Y')
+    for path in extensiondict.extension_dict.values():
+        command = dst_base + path + '/' + year + '/'
+        try:
+            if not os.path.exists(command):
+                print('Does the path exist?', os.path.exists(command))
+                print(command)
+                os.makedirs(command)
+        except FileExistsError:
+            print("Directory already exists; not created")
+
+
+# The src_base_folder is the folder from which all files are copied out of
+src_base = "/home/skaranzx16/Downloads/"  # Enter your source folder path here i.e., the folder you intend to clean up
+# dst_base is the base destination folder off of which the filetype and year based file directory trees begin
+dst_base = "/home/skaranzx16/"  # Enter your base destination folder path here
+
+buildtree()
+cleandesk()
