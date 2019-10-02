769d65d8d450cfce6c7d5ae1950387157708d1c8
Better-Boy <panaroma365@gmail.com>
2019-10-02 17:54:28 +0530

    modularized cmd args and formatted code
    code formatted according to pylint. Getting a rate of 9+ /10 for both the files. First level Modularization done. Tested the code on my local machine ,working fine

--- fileorganizer_linux.py
+++ fileorganizer_linux.py
@@ -1,21 +1,25 @@
-import os, extensiondict, sys
+import os
+import sys
 from datetime import datetime
-from time import strftime
+
+import extensiondict
 
 
-# The src_base_folder is the folder from which all files are copied out of
-src_base = sys.argv[1]  # Enter your source folder path here i.e., the folder you intend to clean up
-# dst_base is the base destination folder off of which the filetype and year based file directory trees begin
-dst_base = sys.argv[2]  # Enter your base destination folder path here
+def cmd_args():
+    src_base = sys.argv[1]  # The src_base is the folder path from which all files are copied out of
+    dst_base = sys.argv[2]
+    # dst_base is the base destination folder path off of which all files are grouped off
 
-if not src_base.endswith('/'):
-    src_base = src_base + '/'
+    if not src_base.endswith('/'):
+        src_base = src_base + '/'
 
-if not dst_base.endswith('/'):
-    dst_base = dst_base + '/'
+    if not dst_base.endswith('/'):
+        dst_base = dst_base + '/'
 
-def cleandesk():
-    
+    return src_base, dst_base
+
+def cleandesk(src_base, dst_base):
+
     for filename in os.listdir(src_base):
         i = 1
 
@@ -23,14 +27,16 @@ def cleandesk():
         root_name = split[0]
         extension = split[1]
         print('The file ', root_name, ' is of the type ', extension)
-        
+
         try:
-            # Dest is the final argument for destination folder that I feed into the file copy method. It is updated with the appropriate filetype_and year-based directories using conditionals below
+       # Dest is the final argument for destination folder that I feed into
+       # the file copy method. It is updated with the appropriate filetype_and
+       # year-based directories using conditionals below
             dest = dst_base + extensiondict.extension_dict[extension.lower()]
             print('The destination folder based on file type is ', dest)
 
             mdate = os.path.getmtime(src_base + filename)
-            year = datetime.fromtimestamp(int(mdate)).strftime("%Y") 
+            year = datetime.fromtimestamp(int(mdate)).strftime("%Y")
             print('The year of modification for this file is', year)
 
             dest = dest + "/" + year
@@ -43,13 +49,17 @@ def cleandesk():
                 os.makedirs(dest)
                 year_exists = os.path.exists(dest)
                 print("The year directory now exists.")
-            
+
             print('The file name is ', filename)
 
             stripped_filename = filename.replace(" ", "\\ ").replace("'", "\\" + "\'")
             print('The stripped file name is ', stripped_filename)
 
-            # file_exists_in_dest checks if the specified directory is a file, which indicates that the filename in fact exists. If it does, the block underneath this line either creates a new name by appending numbers to the end of the filename or leaves the file alone, based on a decision made by the user
+            # file_exists_in_dest checks if the specified directory is a file
+            # which indicates that the filename in fact exists. If it does,
+            # the block underneath this line either creates a new name by
+            # appending numbers to the end of the filename or leaves the
+            # file alone, based on a decision made by the user
 
             file_exists_in_dest = os.path.isfile(dest + '/' + filename)
             print('Does the file already exist in destination? ', file_exists_in_dest)
@@ -61,7 +71,10 @@ def cleandesk():
             print(file_exists_in_src)
 
             if file_exists_in_dest:
-                moveornot = input(filename + ' already exists in the destination directory. \nWould you like me to move the file to the destination after renaming it (or) leave it alone? \nPress 1 for the first option and press any other key to choose the second. ')
+                moveornot = input(filename + ' already exists in the destination directory. \
+                \nWould you like me to move the file to the destination after renaming it (or) \
+                leave it alone? \nPress 1 for the first option and press any other key to choose \
+                the second. ')
 
                 if moveornot == '1':
 
@@ -76,7 +89,7 @@ def cleandesk():
                         file_exists_in_dest = os.path.isfile(dest + '/' + newname)
                         print('A name clash occurred in the process but was fixed.')
                         filename = newname
-                
+
                 else:
                     print('Alright, I\'ll leave it alone')
                     print(filename + ' was not transferred from ' + src_base + ' to ', dest)
@@ -89,12 +102,12 @@ def cleandesk():
 
             if not os.path.isfile(src_base + filename):
                 print(filename, ' was deleted from ', src_base, '\n\n')
-            
+
             else:
                 print(filename, ' was not deleted from ', src_base, '\n\n')
 
-        except Exception:
-            extension.lower() not in extensiondict.extension_dict.keys()
+        except:
+            _ = extension.lower() not in extensiondict.extension_dict.keys()
 
 
 # movefile is the function that handles moving of files from source to destination folder
@@ -104,8 +117,9 @@ def movefile(src, dst):
     os.system(command)
     print("The file was successfully transferred")
 
-# buildtree() is a function that checks whether you have the file organization system in place. If not, it creates the necessary trees for you
-def buildtree():
+# buildtree() is a function that checks whether you have the file organization
+# system in place. If not, it creates the necessary trees for you
+def buildtree(dst_base):
     now = datetime.now()
     year = now.strftime('%Y')
 
@@ -122,6 +136,13 @@ def buildtree():
         except FileExistsError:
             print("Directory already exists; not created")
 
+def run():
+    """
+    Orchestrator to run the functions
+    """
+    src_base, dst_base = cmd_args()
+    buildtree(dst_base)
+    cleandesk(src_base, dst_base)
 
-buildtree()
-cleandesk()
+if __name__ == "__main__":
+    run()
