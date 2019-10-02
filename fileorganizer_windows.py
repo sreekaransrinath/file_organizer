8621daab4a2bc0187cba468da380379ff78e2e45
Better-Boy <panaroma365@gmail.com>
2019-10-02 18:04:46 +0530

    modularized and formatted code
    pylint code rating 9.09/10 for this particular file

--- fileorganizer_windows.py
+++ fileorganizer_windows.py
@@ -1,52 +1,61 @@
 import os
-from datetime import datetime
-from time import strftime
-import extensiondict
 import sys
+from datetime import datetime
 
-# The src_base_folder is the folder from which all files are copied out of
-src_base = sys.argv[1]  # Enter your source folder path here i.e., the folder you intend to clean up
-# dst_base is the base destination folder off of which the filetype and year based file directory trees begin
-dst_base = sys.argv[2]  # Enter your base destination folder path here
+import extensiondict
 
-if not src_base.endswith('/'):
-    src_base = src_base + '/'
 
-if not dst_base.endswith('/'):
-    dst_base = dst_base + '/'
+def cmd_args():
+    src_base = sys.argv[1]  # The src_base is the folder path from which all files are copied out of
+    dst_base = sys.argv[2]
+    # dst_base is the base destination folder path off of which all files are grouped off
 
-def cleandesk():
+    if not src_base.endswith('/'):
+        src_base = src_base + '/'
+
+    if not dst_base.endswith('/'):
+        dst_base = dst_base + '/'
+
+    return src_base, dst_base
+
+def cleandesk(src_base, dst_base):
     for filename in os.listdir(src_base):
         i = 1
-        
+
         split = os.path.splitext(src_base + filename)
         root_name = split[0]
         extension = split[1]
         print('The file ', root_name, ' is of the type ', extension)
-        
+
         try:
-            # Dest is the final argument for destination folder that I feed into the file copy method. It is updated with the appropriate filetype_and year-based directories using conditionals below
+            # Dest is the final argument for destination folder that I feed into the
+            # file copy method. It is updated with the appropriate filetype_and year-based
+            # directories using conditionals below
             dest = dst_base + extensiondict.getpath(extension.lower())
             print('The destination folder based on file type is ', dest)
-            
+
             cdate = os.path.getctime(src_base + filename)
             year = datetime.fromtimestamp(int(cdate)).strftime('%Y')
             print('The year of creation for this file is ', year)
-            
+
             dest = dest + "/" + year
             print("Destination folder with year added is", dest)
-            
+
             year_exists = os.path.exists(dest)
             print("Does the appropriate year directory exist? ", year_exists)
-            
+
             if not year_exists:
                 os.makedirs(dest)
                 year_exists = os.path.exists(dest)
                 print("The year directory now exists.")
-            
+
             print("The filename is ", filename)
-            
-            # file_exists_in_dest checks if the specified directory is a file, which indicates that the filename in fact exists. If it does, the block underneath this line either creates a new name by appending numbers to the end of the filename or leaves the file alone, based on a decision made by the user
+
+            # file_exists_in_dest checks if the specified directory is a file,
+            # which indicates that the filename in fact exists. If it does,
+            # the block underneath this line either creates a new name by appending
+            # numbers to the end of the filename or leaves the file alone,
+            # based on a decision made by the user
 
             file_exists_in_dest = os.path.isfile(dest + '/' + filename)
             print('Does the file already exist in destination? ', file_exists_in_dest)
@@ -57,7 +66,10 @@ def cleandesk():
             file_exists_in_src = os.path.isfile(filepath)
 
             if file_exists_in_dest:
-                moveornot = input(filename + ' already exists in the destination directory. \nWould you like me to move the file to the destination after renaming it (or) leave it alone? \nPress 1 for the first option and press any other key to choose the second. ')
+                moveornot = input(filename + ' already exists in the destination directory. \
+                \nWould you like me to move the file to the destination after renaming \
+                it (or) leave it alone? \nPress 1 for the first option and press any \
+                other key to choose the second. ')
 
                 if moveornot == '1':
                     while file_exists_in_dest:
@@ -81,13 +93,13 @@ def cleandesk():
                     movefile(src_base, filename, dest)
 
             if not os.path.isfile(src_base + filename):
-                print(filename, ' was deleted from {', src_base + filename,'}', src_base, '\n\n')
+                print(filename, ' was deleted from {', src_base + filename, '}', src_base, '\n\n')
 
             else:
                 print(filename, ' was not deleted from ', src_base, '\n\n')
-                    
+
         except Exception:
-            extension.lower() not in extensiondict.extension_dict.keys()
+            _ = extension.lower() not in extensiondict.extension_dict.keys()
 
 
 def movefile(src, filename, dst):
@@ -99,8 +111,9 @@ def movefile(src, filename, dst):
     os.system(command)
     print(filename, ' was transferred from ', src, ' to ', dst)
 
-# buildtree() is a function that checks whether you have the file organization system in place. If not, it creates the necessary trees for you
-def buildtree():
+# buildtree() is a function that checks whether you have the file organization
+# system in place. If not, it creates the necessary trees for you
+def buildtree(dst_base):
     now = datetime.now()
     year = now.strftime('%Y')
     for path in extensiondict.extension_dict.values():
@@ -113,5 +126,10 @@ def buildtree():
         except FileExistsError:
             print("Directory already exists; not created")
 
-buildtree()
-cleandesk()
+def run():
+    src_base, dst_base = cmd_args()
+    buildtree(dst_base)
+    cleandesk(src_base, dst_base)
+
+if __name__ == "__main__":
+    run()
